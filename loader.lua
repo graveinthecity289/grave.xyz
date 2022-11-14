1e6a9f1d3d6fa53972b97c0e8f32b42fec2f905c
graveinthecity289 <99278679+graveinthecity289@users.noreply.github.com>
2022-11-13 23:01:17 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,10 @@
+local xyz = {
+  [7525610732] = ''
+}
+
+if xyz[game.PlaceId] then
+  for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.Idled)) do
+    v:Disable()
+  end
+   pcall(function() return loadstring(game:HttpGet(xyz[game.PlaceId]))() end);
+end
